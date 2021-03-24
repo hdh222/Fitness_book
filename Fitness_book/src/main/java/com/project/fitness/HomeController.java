4d8c91b18da8e6d7a1273f2cd5610a0ebf9942cb
ghdej <ghdej@DESKTOP-M1TQRAQ>
2021-03-24 12:54:12 +0900

    2021.03.24 <추가> 프로젝트 기본 구성

---
+++ Fitness_book/src/main/java/com/project/fitness/HomeController.java
@@ -0,0 +1,14 @@
+package com.project.fitness;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping(value="/home")
+	public String home() {
+		return "home";
+	}
+}
